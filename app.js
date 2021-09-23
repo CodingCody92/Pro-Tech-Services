753257f4410e31494525273c7f64b057f17a3dc2
Cody <alexhill318@yahoo.com>
2021-09-23 16:48:07 -0500

    Pro Tech

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementsByClassName("navbar").style.top = "0";
+    } else {
+        document.getElementsByClassName("navbar").style.top = "-50px";
+    }
+}
